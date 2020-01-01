7d10913f7a56712fa7e59da9f551f003bc5c8cd7
Osaiteh <26644393+Osaiteh@users.noreply.github.com>
2020-01-02 00:57:55 +0100

    html and css link; js functions
    I added a link to the JavaScript and Css files to html
    I added some js functions

--- clock.js
+++ clock.js
@@ -3,4 +3,27 @@ function currentTime(){
     var hour = date.getHours();
     var min = date.getMinutes();
     var sec = date.getSeconds();
+    var midday = "AM";
 }
+function currentTime(){
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    midday = (hour>= 12) ? "PM" : "AM";
+    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour);
+}
+function updateTime(k){
+    if(k < 10) {
+        return "0" + k;
+    }
+    else {
+        return k;
+    }
+}
+document.getElementById("clock").innerHTML = hour + " : " + min + " : " + sec + midday;
+function currentTime(){
+    var t = setTimeout(function(){
+        currentTime()
+    }, 1000);
+}
+currentTime;
