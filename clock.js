83c1dfa2887b41c79859acc064ec5fd58644f8ed
Adesanoye Samson <adesanoye.samson@yahoo.com>
2020-01-02 17:38:11 -0800

    rewrite necessary part of the code thanks to codepen

--- clock.js
+++ clock.js
@@ -1,29 +1,27 @@
-function currentTime(){
+function showTimee() {
     var date = new Date();
     var hour = date.getHours();
     var min = date.getMinutes();
     var sec = date.getSeconds();
     var midday = "AM";
-}
-function currentTime(){
-    hour = updateTime(hour);
-    min = updateTime(min);
-    sec = updateTime(sec);
-    midday = (hour>= 12) ? "PM" : "AM";
-    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour);
-}
-function updateTime(k){
-    if(k < 10) {
-        return "0" + k;
+    // first 2 -6 lines are right and okay
+    if(hour == 0){
+        hour = 12
     }
-    else {
-        return k;
+    if(hour > 12) {
+        hour = hour - 12;
+        midday = "PM";
     }
+    // short for if else statement (forgotten d name also)
+    hour = (hour < 10) ? "0" + hour : hour;
+    min = (min < 10) ? "0" + min : min;
+    sec = (sec < 10) ? "0" + sec : sec;
+
+    var time = hour + ":" + min + ":" + sec + " " + midday;
+    // replace the inner text of the elemt id 
+    document.getElementById("clock").innerText = time;
+    // have forgotten d name they call it function in a function. 
+    setTimeout(showTimee, 1000); 
+
 }
-document.getElementById("clock").innerHTML = hour + " : " + min + " : " + sec + midday;
-function currentTime(){
-    var t = setTimeout(function(){
-        currentTime()
-    }, 1000);
-}
-currentTime;
+showTimee()
